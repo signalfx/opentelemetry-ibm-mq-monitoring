c8d6d03468eec00be4a68b8a72e392d5b3a80f51
Jason Plumb <jplumb@splunk.com>
2025-04-29 14:50:56 -0700

    add javadoc and simplify

--- src/main/java/com/appdynamics/extensions/webspheremq/metricscollector/IntAttributesBuilder.java
+++ src/main/java/com/appdynamics/extensions/webspheremq/metricscollector/IntAttributesBuilder.java
@@ -2,13 +2,24 @@ package com.appdynamics.extensions.webspheremq.metricscollector;
 
 import com.appdynamics.extensions.webspheremq.config.WMQMetricOverride;
 
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.Map;
 
+/**
+ * This class is responsible for building a list of integer-valued attributes
+ * from a predefined collection of MWQMetricOverride metrics. It does this
+ * by allocating a new int[] and appending the predefined values to
+ * an array of input values.
+ */
 final class IntAttributesBuilder {
-    private final Map<String, WMQMetricOverride> metrics;
+    private final Collection<WMQMetricOverride> metrics;
 
     IntAttributesBuilder(Map<String, WMQMetricOverride> metrics) {
+        this(metrics.values());
+    }
+
+    IntAttributesBuilder(Collection<WMQMetricOverride> metrics) {
         this.metrics = metrics;
     }
 
@@ -19,7 +30,7 @@ final class IntAttributesBuilder {
 
         // fill attrs from metrics.
         int i = inputAttrs.length;
-        for (WMQMetricOverride metric : metrics.values()) {
+        for (WMQMetricOverride metric : metrics) {
             attrs[i++] = metric.getConstantValue();
         }
 
