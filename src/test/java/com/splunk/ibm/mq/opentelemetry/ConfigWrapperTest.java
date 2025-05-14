fd95f0a87ecad85c31d603c19f0545ac3a5f0e14
Jason Plumb <jplumb@splunk.com>
2025-05-14 08:21:02 -0700

    simplify test

--- src/test/java/com/splunk/ibm/mq/opentelemetry/ConfigWrapperTest.java
+++ src/test/java/com/splunk/ibm/mq/opentelemetry/ConfigWrapperTest.java
@@ -21,35 +21,43 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import java.io.FileNotFoundException;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ConfigWrapperTest {
 
+  String file;
+
+  @BeforeEach
+  void setUp() {
+    file = ConfigWrapperTest.class.getResource("/conf/config.yml").getFile();
+  }
+
   @Test
   void testQueueManagerNames() throws FileNotFoundException {
-    ConfigWrapper config =
-        ConfigWrapper.parse(ConfigWrapperTest.class.getResource("/conf/config.yml").getFile());
+    ConfigWrapper config = ConfigWrapper.parse(file);
     assertThat(config.getQueueManagerNames()).isEqualTo(singletonList("QM1"));
   }
 
   @Test
   void testNumberOfThreads() throws FileNotFoundException {
     ConfigWrapper config =
-        ConfigWrapper.parse(ConfigWrapperTest.class.getResource("/conf/config.yml").getFile());
+        ConfigWrapper.parse(file);
     assertThat(config.getNumberOfThreads()).isEqualTo(1);
   }
 
   @Test
   void testTaskDelay() throws FileNotFoundException {
     ConfigWrapper config =
-        ConfigWrapper.parse(ConfigWrapperTest.class.getResource("/conf/config.yml").getFile());
+        ConfigWrapper.parse(file);
     assertThat(config.getTaskDelay()).isEqualTo(Duration.of(27, ChronoUnit.SECONDS));
   }
 
   @Test
   void testTaskInitialDelay() throws FileNotFoundException {
     ConfigWrapper config =
-        ConfigWrapper.parse(ConfigWrapperTest.class.getResource("/conf/config.yml").getFile());
+        ConfigWrapper.parse(file);
     assertThat(config.getTaskInitialDelaySeconds()).isEqualTo(0);
   }
 }
