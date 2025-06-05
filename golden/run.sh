669079b6d0a6410a98c2e17df347ec6306876b48
Jason Plumb <jplumb@splunk.com>
2025-06-05 14:32:37 -0700

    remove troubleshooting gibblets

--- golden/run.sh
+++ golden/run.sh
@@ -3,7 +3,6 @@
 set -eu
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-echo $SCRIPT_DIR
 java -cp $SCRIPT_DIR/../build/libs/ibm-mq-monitoring-0.1.0-all.jar:$SCRIPT_DIR/../build/libs/com.ibm.mq.allclient.jar \
   -Dotel.logs.exporter=none -Dotel.traces.exporter=none \
   com.splunk.ibm.mq.opentelemetry.Main $SCRIPT_DIR/config.yml
