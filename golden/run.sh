58f24d2912b1a4ea3bd3d6cfe7975af980bbfb84
Jason Plumb <jplumb@splunk.com>
2025-06-05 14:06:27 -0700

    copy ibm jar local for e2e, fix classpaths.

--- golden/run.sh
+++ golden/run.sh
@@ -3,7 +3,7 @@
 set -eu
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-java -cp $SCRIPT_DIR/../target/ibm-mq-monitoring-extension-opentelemetry.jar:$SCRIPT_DIR/../target/libs/com.ibm.mq.allclient.jar \
+echo $SCRIPT_DIR
+java -cp $SCRIPT_DIR/../build/libs/ibm-mq-monitoring-0.1.0-all.jar:$SCRIPT_DIR/../build/libs/com.ibm.mq.allclient.jar \
   -Dotel.logs.exporter=none -Dotel.traces.exporter=none \
   com.splunk.ibm.mq.opentelemetry.Main $SCRIPT_DIR/config.yml
